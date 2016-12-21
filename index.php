dd1111d6e9a83dcb404f058aedeafd05a0e52fc2
Jon II <jonathan.gustaf.cilli@gmail.com>
2016-12-21 13:15:35 +0100

    Grunden
    Jag lägger upp grunden för hemsidan

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<link href="stylesheets/style.css" rel='stylesheet' type='text/css'>	
+	</head>
+	<body>
+		<p> TESTING THIS FUCKING REVOLTING THING </p>
+		<h1> SO, THIS LOOKS GOOD </h1>
+	</body>
+</html>
